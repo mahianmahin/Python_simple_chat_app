38e9813b3130e9598fdf59b5f7f19bc507045fba
Mahian Mahin <80479590+mahianmahin@users.noreply.github.com>
2021-11-18 17:04:27 +0600

    Update client.py

--- client.py
+++ client.py
@@ -1,42 +1,42 @@
-# import socket
-# import threading
+import socket
+import threading
 
-# nickname = input("Nickname: ")
-# ip = input("Server Ip address >>> ")
+nickname = input("Nickname: ")
+ip = input("Server Ip address >>> ")
 
-# # SERVER = '35.173.69.207'
-# SERVER = str(ip)
-# # SERVER = '192.168.56.1'
-# PORT = 4545
+# SERVER = '35.173.69.207'
+SERVER = str(ip)
+# SERVER = '192.168.56.1'
+PORT = 4545
 
-# print("[+] Waiting to connect to the server...")
+print("[+] Waiting to connect to the server...")
 
 
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect((SERVER, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER, PORT))
 
-# def send():
-#     while True:
-#         msg = input("")
+def send():
+    while True:
+        msg = input("")
 
-#         if msg == "OFF":
-#             client.send(msg.encode('utf-8'))
-#             client.close()
-#             break
+        if msg == "OFF":
+            client.send(msg.encode('utf-8'))
+            client.close()
+            break
 
-#         else:
-#             msg = f'{nickname}: ' + str(msg)
-#             client.send(msg.encode('utf-8'))
+        else:
+            msg = f'{nickname}: ' + str(msg)
+            client.send(msg.encode('utf-8'))
         
 
-# def recieve():
-#     while True:
-#         msg = client.recv(1024).decode('utf-8')
-#         print(str(msg))
+def recieve():
+    while True:
+        msg = client.recv(1024).decode('utf-8')
+        print(str(msg))
 
 
-# recieve_thread = threading.Thread(target=recieve)
-# recieve_thread.start()
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
     
-# send_thread = threading.Thread(target=send)
-# send_thread.start()
+send_thread = threading.Thread(target=send)
+send_thread.start()
