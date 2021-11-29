1abf9dc8fa8ee0daa51e5c1f676664085958b691
Mahian Mahin <80479590+mahianmahin@users.noreply.github.com>
2021-11-29 19:26:40 +0600

    Update server.py

--- server.py
+++ server.py
@@ -1,48 +1,48 @@
-# import socket
-# import threading
+import socket
+import threading
 
-# nickname = input("Nickname: ")
+nickname = input("Nickname: ")
 
-# # HOST = socket.gethostbyname(socket.gethostname())
-# HOST = '192.168.0.102'
-# # HOST = '127.0.0.1'
-# PORT = 4545
+# HOST = socket.gethostbyname(socket.gethostname())
+HOST = '192.168.0.102'
+# HOST = '127.0.0.1'
+PORT = 4545
 
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind((HOST, PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
 
-# server.listen()
-# print(f'[+] Server is listening with {HOST} on {PORT}...')
+server.listen()
+print(f'[+] Server is listening with {HOST} on {PORT}...')
 
-# client, address = server.accept()
-# client.send(f'[+] Connection established with {HOST} on port {PORT}\n'.encode('utf-8'))
+client, address = server.accept()
+client.send(f'[+] Connection established with {HOST} on port {PORT}\n'.encode('utf-8'))
 
-# print(f'[+] Connected to {address[0]} on port {PORT}\n')
+print(f'[+] Connected to {address[0]} on port {PORT}\n')
 
-# def send():
-#     while True:
-#         msg = input("")
+def send():
+    while True:
+        msg = input("")
 
-#         if msg == "OFF":
-#             client.send(msg.encode('utf-8'))
-#             client.close()
-#             break
+        if msg == "OFF":
+            client.send(msg.encode('utf-8'))
+            client.close()
+            break
 
-#         else:
-#             msg = f'{nickname}: ' + str(msg)
-#             client.send(msg.encode('utf-8'))
+        else:
+            msg = f'{nickname}: ' + str(msg)
+            client.send(msg.encode('utf-8'))
 
-# def recieve():
-#     while True:
-#         msg = client.recv(1024).decode('utf-8')
-#         print(str(msg))
+def recieve():
+    while True:
+        msg = client.recv(1024).decode('utf-8')
+        print(str(msg))
 
 
-# send_thread = threading.Thread(target=send)
-# send_thread.start()
+send_thread = threading.Thread(target=send)
+send_thread.start()
 
-# recieve_thread = threading.Thread(target=recieve)
-# recieve_thread.start()
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
 
 
 
